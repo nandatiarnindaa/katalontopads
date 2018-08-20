c1187e913531a6d1908270e6608bd9c90ec044d8
Nakama <Nakama@201060->
2018-08-20 15:29:05 +0700

    add negative test create group with invalid chars

---
+++ Object Repository/error_group_name_invalid_char.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>error_group_name_invalid_char</name>
+   <tag></tag>
+   <elementGuidId>c30e747b-ff6d-4878-bdc7-51737c7c29b4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;add-new-group-promo&quot;]//*[contains(text(),&quot;Nama grup tidak valid&quot;)]</value>
+   </webElementProperties>
+</WebElementEntity>
--- Test Suites/Dashboard.ts
+++ Test Suites/Dashboard.ts
@@ -4,10 +4,35 @@
    <name>Dashboard</name>
    <tag></tag>
    <isRerun>false</isRerun>
+   <lastRun>2018-08-20T15:19:55</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>aafa44ff-8485-4bb5-9066-c768d2fb3275</testSuiteGuid>
+   <testCaseLink>
+      <guid>193d3bc2-d2f8-40fc-86c6-7f4d1468f863</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dashboard - Negative - Create New Group with Group Name Empty</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a66dd679-1c30-4504-9cc4-02917cfcead5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dashboard - Negative - Create New Group With Group Name More Than 70 Chars</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>eea68844-dfd0-4855-85cd-5942ca22758d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dashboard - Negative - Create New Group With Group Name with Invalid Chars</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>41c82420-a74b-43d4-933d-430178515846</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dashboard - Positive - Create New Group</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
